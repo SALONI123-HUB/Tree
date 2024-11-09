273760a74c04f8c90ed44f377a245785b6f3c9bf
SALONI SRIVASTAVA <salonisrivastava127@gmail.com>
2024-11-09 17:23:33 +0530

    Create HeightOfTree.cpp

---
+++ HeightOfTree.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm> // For max
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return max(left, right) + 1;
+    }
+};
+
+int main() {
+    // Creating a sample binary tree
+    //      1
+    //     / \
+    //    2   3
+    //   / \
+    //  4   5
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    Solution solution;
+    int depth = solution.maxDepth(root);
+    cout << "Maximum Depth: " << depth << endl;
+
+    // Free the allocated memory
+    delete root->left->left;
+    delete root->left->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+
+    return 0;
+}
